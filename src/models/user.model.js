4f3e10ab7e55d2d5cf200f196ac6628209d8e35e
Yash Kumar <13102003y@gmail.com>
2025-01-15 18:08:08 +0530

    user model updated

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         required : true,
     },
-    fullname:{
+    fullName:{
         type : String,
         required : true,
         index: true,
@@ -59,6 +59,30 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.isPasswordCorrect = async function
 (password){
     return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+)
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id: this._id,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+    )
 }
 
 export const User = mongoose.model("User", userSchema)
