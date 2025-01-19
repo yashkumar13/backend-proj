63e9489ca9275d22a31bc4399d6995cec863ed2c
Yash Kumar <13102003y@gmail.com>
2025-01-19 09:08:45 +0530

    set up login and logout controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,8 +3,26 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {User} from "../models/user.model.js";                    
+
+
+const generateAccessAndRefreshTokens = async(userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+
+        return {accessToken, refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong white generating refresh and access token")
+    }
+}
+
 const registerUser = asyncHandler( async (req, res) => {
-    //get user detailws from frontend
+    //get user details from frontend
     //validation - not empty
     //check if user already exists - username, email
     //check for images, check for avatar
@@ -79,4 +97,96 @@ const registerUser = asyncHandler( async (req, res) => {
     
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async (req, res) => {
+    //get user details from frontend
+    //username or email
+    //check if user exists or not - username, email
+    //check password - compare with password saved in db  
+    //generate access and refresh token
+    //send cookie
+    //retrun response
+
+
+    const { email , username , password} = req.body
+
+    if(!username || !email){
+        throw new ApiError(400, "username or email is required.");
+    }
+
+    const user = await User.findOne({
+        $or: [{email}, {username}]
+    })
+
+    if(!user){
+        throw new ApiError(404,"User does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(401,"Invalid user credentials")
+    }
+
+    const {accessToken , refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).
+    select("-password -refreshToken")
+
+    //sending cookies
+    const options = {
+        httpOnly: true,
+        secure: true
+    }// can be modified by server
+
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken.options)
+    .cookie("refreshToken", refreshToken, options).json(
+        new ApiResponse(
+            200,{
+                user: loggedInUser, accessToken, refreshToken
+                //if user is trying to save tokens in local storage
+            },
+            "User logged in successfully"
+
+        )
+    )
+
+
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(
+        new ApiResponse(200
+            ,{}
+            ,"User logged out Successfully"
+        )
+    )
+})
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import {registerUser} from "../controllers/user.controller.js";
+import {registerUser,loginUser} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-
+import {verifyJWT} from "../middlewares/auth.middleware.js"
 const router = Router();
 
 router.route("/register").post(
@@ -17,6 +17,11 @@ router.route("/register").post(
     ]),
     registerUser
 )
+
+router.route("/login").post(loginUser)
+
+//secured route
+router.route("/logout").post(verifyJWT ,logoutUser)
 
 
 export default router
